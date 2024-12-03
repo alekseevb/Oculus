3899776cde63987e35c03a50a6af3c564f4ebaf6
Алексей Алексеев <alexseev617@gmail.com>
2024-12-03 19:40:52 +0300

    Доделал адаптив, сайт закончен

--- js/main.js
+++ js/main.js
@@ -3,6 +3,11 @@ const CONTENT_WHEN_ACTIVE = 'tabs__content-item_active';
 
 const tabItem = document.querySelectorAll('.tabs__btn-item');
 const tabContent = document.querySelectorAll('.tabs__content-item');
+const openBurgerMenu = document.getElementById('openBurgerMenu')
+const closeBurgerMenu = document.getElementById('closeBurgerMenu');
+const burgerMenu = document.getElementById('burgerMenu');
+const burgerContent = document.getElementById('burgerContent')
+const body = document.querySelector('body')
 
 const open = (e) => {
     const tabTarget = e.currentTarget;
@@ -22,7 +27,28 @@ const open = (e) => {
     pressBtn.classList.add(CONTENT_WHEN_ACTIVE);
 }
 
+const burgerOpenHandler = () => {
+    burgerMenu.classList.add('burger-menu_hidden');
+    body.classList.add('body_fixed');
+}
+
+const burgerCloseHandler = () => {
+    burgerMenu.classList.remove('burger-menu_hidden');
+    body.classList.remove('body_fixed');
+}
+
+burgerMenu.addEventListener('click', (event) => {
+    const isClickOutsideContent = !event.composedPath().includes(burgerContent)
+
+    if (isClickOutsideContent) {
+        burgerCloseHandler();
+    }
+})
+
 
 tabItem.forEach(function (element) {
     element.addEventListener('click', open);
 })
+
+openBurgerMenu.addEventListener('click', burgerOpenHandler);
+closeBurgerMenu.addEventListener('click', burgerCloseHandler)
